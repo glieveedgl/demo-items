c8cbb947fdf7b2519f8223643d5ffce65253e3e0
glieveedgl <53789290+glieveedgl@users.noreply.github.com>
2025-04-17 17:00:44 +0300

    Done

---
+++ src/components/items/grid.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex flex-wrap">
+    <slot />
+  </div>
+</template>
---
+++ src/components/items/selected.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps, useSlots } from 'vue'
+import type { TItem } from '@/entities/Item.ts'
+const slots = useSlots()
+const props = defineProps<{
+  value: TItem[] | TItem | any
+}>()
+</script>
+<template>
+  <div v-if="slots.content">
+    <slot name="content" v-if="Array.isArray(value) ? value.length : value" v-bind="{ value }" />
+  </div>
+  <div v-else>{{ value }}</div>
+</template>
